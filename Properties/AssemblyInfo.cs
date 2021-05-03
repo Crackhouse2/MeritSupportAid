5443cb650dd80381e7483f36ab6799c0ec7e72ca
Simon Marchant <crackhouse2@live.com>
2021-05-03 16:01:44 +0100

    update to assembly info

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MeritSupportAid")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Support Tools")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("C02 Software")]
 [assembly: AssemblyProduct("MeritSupportAid")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
